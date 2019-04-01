2ac8744d7898960cf05ea0ca817bb7cf6f5f4d08
Clint.Network <me@clint.network>
2019-04-02 01:48:43 +0200

    Add login view model

---
+++ Librum/ViewModels/Login.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Librum.ViewModels
+{
+    public class Login
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
