e441dbddc7434e7718345167ec2e414a5e42e88f
Clint.Network <me@clint.network>
2019-04-01 20:40:00 +0200

    Implement Article model

---
+++ Models/Article.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Librum.Models
+{
+    public class Article
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string Keywords { get; set; }
+        public string Content { get; set; }
+        public DateTime WritedDatetime { get; set; }
+        public bool IsDraft { get; set; } = false;
+        public string AuthorUsername { get; set; }
+    }
+}
