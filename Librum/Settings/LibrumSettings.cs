fefeecfe9a6732d0ef74cce370c7fe40c92b2051
Clint.Network <me@clint.network>
2019-04-01 22:56:15 +0200

    Add menu w/ configuration

--- Librum/Settings/LibrumSettings.cs
+++ Librum/Settings/LibrumSettings.cs
@@ -8,8 +8,15 @@ namespace Librum.Settings
         public string Icon { get; set; }
         public string CanonicalUri { get; set; }
         public object Author { get; set; }
+        public MenuItem[] Menu { get; set; }
         public object Configuration { get; set; }
         public object UsersStore { get; set; }
         public string DisqusHostname { get; set; }
+    }
+
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
     }
 }
