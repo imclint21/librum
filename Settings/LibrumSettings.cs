ecfa167f458ffbab2b58a2d594ef794a1c8b3f0f
Clint.Network <me@clint.network>
2019-04-01 21:13:05 +0200

    Finish Librum Settings

--- Settings/LibrumSettings.cs
+++ Settings/LibrumSettings.cs
@@ -4,9 +4,11 @@ namespace Librum.Settings
     {
         public string BlogName { get; set; }
         public string Description { get; set; }
+        public string Keywords { get; set; }
         public string Icon { get; set; }
-        public string AuthorName { get; set; }
-        public string Twitter { get; set; }
-        public string Website { get; set; }
+        public string CanonicalUri { get; set; }
+        public object Author { get; set; }
+        public object UsersStore { get; set; }
+        public string DisqusHostname { get; set; }
     }
 }
