9499df2abc6dbe039f081f1f91eac5bb273fbf0a
Clint.Network <me@clint.network>
2019-04-02 01:35:02 +0200

    Set default port

--- Librum/Program.cs
+++ Librum/Program.cs
@@ -24,6 +24,7 @@ namespace Librum
                     config.SetBasePath(Directory.GetCurrentDirectory());
                     config.AddJsonFile(args.Count() > 0 ? args[0] : "librum.json", optional: false, reloadOnChange: true);
                 })
+                .UseUrls("http://[::]:3100/")
                 .UseStartup<Startup>();
     }
 }
