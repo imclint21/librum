9a0e7e120a88d6bd672a3e5e486f305c1c73e8b0
Clint.Network <me@clint.network>
2019-04-01 22:09:26 +0200

    Implement error page

--- Startup.cs
+++ Startup.cs
@@ -69,7 +69,7 @@ namespace Librum
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseStatusCodePagesWithRedirects("/error?errorCode={0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 // app.UseHsts();
             }
