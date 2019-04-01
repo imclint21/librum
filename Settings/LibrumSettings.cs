91f1d675f5955db3789a4e31f3fe6abec8faefec
Clint.Network <me@clint.network>
2019-04-01 19:47:30 +0200

    Add Librum Settings

---
+++ Settings/LibrumSettings.cs
@@ -0,0 +1,12 @@
+namespace Librum.Settings
+{
+    public class LibrumSettings
+    {
+        public string BlogName { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string AuthorName { get; set; }
+        public string Twitter { get; set; }
+        public string Website { get; set; }
+    }
+}
