e12cf377eff91639eb3ba02c0bb172a3d1713f49
Clint.Network <me@clint.network>
2019-04-02 01:48:36 +0200

    Add user store model

---
+++ Librum/Models/UserStoreItem.cs
@@ -0,0 +1,13 @@
+namespace Librum.Models
+{
+    public class UserStoreItem
+    {
+        public string Role { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string Twitter { get; set; }
+        public string GitHub { get; set; }
+        public string Website { get; set; }
+    }
+}
