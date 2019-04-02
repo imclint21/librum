34c854dc0a4e9a74bbd6c442fbd880114c8dcc59
Clint.Network <me@clint.network>
2019-04-02 02:36:14 +0200

    Add required attribute for article model

--- Librum/Models/Article.cs
+++ Librum/Models/Article.cs
@@ -1,14 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Librum.Models
 {
     public class Article
     {
         public string Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Slug { get; set; }
+
+        // [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Keywords { get; set; }
+
+        [Required]
         public string Content { get; set; }
         public DateTime WritedDatetime { get; set; }
         public bool IsDraft { get; set; } = false;
