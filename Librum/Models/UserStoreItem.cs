ff2862e7f80163f5e2950233db25c122cfa816b2
Clint.Network <me@clint.network>
2019-04-02 16:04:12 +0200

    Add bio to librum.json

--- Librum/Models/UserStoreItem.cs
+++ Librum/Models/UserStoreItem.cs
@@ -6,6 +6,7 @@ namespace Librum.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public string FullName { get; set; }
+        public string Bio { get; set; }
         public string Twitter { get; set; }
         public string GitHub { get; set; }
         public string Website { get; set; }
