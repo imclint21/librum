eafaa9c8b421b7847b297290728824f3235f4d4e
Clint.Network <me@clint.network>
2019-05-07 05:35:39 +0200

    Display only non-drafts

--- Librum/Controllers/HomeController.cs
+++ Librum/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace Librum.Controllers
         public async Task<IActionResult> Index()
         {
             var articles = await _articles.GetAllAsync();
+            if(!User.Identity.IsAuthenticated)
+            {
+                articles = articles.Where(x => !x.IsDraft).ToList();
+            }
             return View(articles.OrderByDescending(x => x.WritedDatetime).ToList());
         }
 
