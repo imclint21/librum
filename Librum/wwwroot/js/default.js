29d30e4d0ce68f2b636463ac0690b2e9107d5b60
Clint.Network <me@clint.network>
2019-07-20 22:39:35 +0200

    Add anchors and scroll on click feature

--- Librum/wwwroot/js/default.js
+++ Librum/wwwroot/js/default.js
@@ -1,1 +1,17 @@
+// Load feather-icons
 feather.replace();
+
+// On document ready
+$(document).ready(function () {
+    $(".markdown").find("h2,h3,h4,h5,h6").addClass("clickable-link")
+
+    $(".clickable-link").click(function () {
+        if (history.pushState) {
+            history.pushState(null, null, "#" + $(this).attr("id"));
+        }
+        else {
+            location.hash = $(this).attr("id");
+        }
+        $('html, body').animate({ scrollTop: $(this).position().top }, "slow");
+    });
+})
